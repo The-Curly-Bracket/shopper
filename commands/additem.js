f660766272dc68d27413aa5002d327cbfcc5939e
EliTheCoder <ex.games.org@gmail.com>
2019-01-28 19:12:50 -0600

    added additem command

--- commands/additem.js
+++ commands/additem.js
@@ -1,5 +1,18 @@
-module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
+const fs = require("fs");
+const eliapi = require("eliapi");
 
+module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
+  if (args.length != 2) return "Invalid command usage";
+  if (args[0] == "finish") return "You cannot name a product \"finish\"";
+  if (parseInt(args[1]) < 0) return "Cost must be positive";
+  if (Number.isInteger(parseInt(args[0]))) return "Cost must be a whole number";
+  let shop = require("./shop.json");
+  shop[args[0]] = parseInt(args[1]);
+  fs.writeFileSync(file, JSON.stringify(p, null, 2), "utf8", {
+    if (err) {
+      return eliapi.log(2, err);
+    }
+  });
   return 0; // nonzero value is returned in reply
 }
 
