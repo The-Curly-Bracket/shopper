00eac6e028c3a0622859a1c9b688e58355f07328
AlexCheese <alexip013@gmail.com>
2019-01-28 19:53:34 -0600

    order listener

--- commands/order.js
+++ commands/order.js
@@ -1,8 +1,19 @@
 module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
-  message.channel.send(`Type the name of the unit you wish to order, then the quantity!`);
-	message.channel.awaitMessages(msg => {
-
-	});
+	let itemListener = input => {
+		if (input.author.username != message.author.username) return;
+		if (input.content.startsWith('$order')) bot.off('message', itemListener);
+		let cont = input.content.split(" ")
+		if (cont[0] == 'finish') {
+			message.channel.send('okay bye');
+			bot.off('message', itemListener);
+		}
+		if (shop[cont[0]]) {
+			if (!cont[1]) cont[1] = 1;
+			message.channel.send(`Ordering ${cont[1]} ${cont[0]}!`);
+		}
+	}
+	await message.channel.send(`Type the name of the unit you wish to order, then the quantity!`);
+	bot.on('message', itemListener);
   return 0; // nonzero value is returned in reply
 }
 
