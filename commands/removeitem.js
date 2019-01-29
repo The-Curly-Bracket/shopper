03ee640b7ee37adec408c702f6c5d041a7402319
EliTheCoder <ex.games.org@gmail.com>
2019-01-28 19:40:07 -0600

    added remove command

--- commands/removeitem.js
+++ commands/removeitem.js
@@ -1,5 +1,16 @@
-module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
+const fs = require("fs");
+const eliapi = require("eliapi");
 
+module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
+  if (args.length != 1) return "Invalid command usage";
+  if (!shop[args[0]]) return "Item does not exist";
+  delete shop[args[0]];
+  fs.writeFileSync("shop.json", JSON.stringify(shop, null, 2), "utf8", {
+    if (err) {
+      return eliapi.log(2, err);
+    }
+  });
+  message.channel.send(`Item "${args[0]}" removed from shop`);
   return 0; // nonzero value is returned in reply
 }
 
