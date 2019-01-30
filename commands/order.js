183d0414ba8b1c3b33834fab1b2b1883d96c3b7c
AlexCheese <alexip013@gmail.com>
2019-01-29 19:14:19 -0600

    embed generator yeet

--- commands/order.js
+++ commands/order.js
@@ -1,6 +1,7 @@
+const Discord = require('discord.js')
 module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
 	let order = {};
-	let itemListener = input => {
+	let itemListener = async input => {
 		if (input.author.username != message.author.username) return;
 		let msg = input.content.toLowerCase();
 		let cont = msg.split(" ");
@@ -20,9 +21,29 @@ module.exports.run = async (bot, message, args, origin, shop) => { // Runs when 
 			case 'finish':
 				message.channel.send('finished order');
 				let i = '';
-				for (let j in order)
-					i += `${order[j]} ${j}s: \$${shop[j]*order[j]}\n`;
-				message.channel.send(i);
+				let prompt = await new Discord.RichEmbed({
+					"title": "__**Unit Order**__",
+					"author": {
+						"name": `${message.author.username}`,
+						"icon_url": `${message.author.avatarURL}`
+					},
+					"timestamp": `${new Date()}`,
+					"footer": {
+						"text": "The Curly Bracket",
+						"icon_url": "https://avatars3.githubusercontent.com/u/46971424?s=200&v=4",
+					}
+				});
+				let total = 0;
+				for (let k in order) {
+					await prompt.addField(`${order[k]} ${k}s`, `\$${shop[k]*order[k]}`, false);
+					total += shop[k]*order[k];
+				}
+				await prompt.addField(`__Total Cost__`, `**$${total}**`);
+				await message.channel.send(prompt)
+					.then(async msg => {
+						await msg.react('✅');
+						await msg.react('❌');
+					});
 				bot.off('message', itemListener);
 				return;
 				break;
