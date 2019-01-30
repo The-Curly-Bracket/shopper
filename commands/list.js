8b3d1cf5170fdfe42347fa61b105a308990026ac
EliTheCoder <ex.games.org@gmail.com>
2019-01-29 19:38:16 -0600

    added list command

---
+++ commands/list.js
@@ -0,0 +1,38 @@
+module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
+  let products = Object.keys(shop).join("\n");
+  let prices = Object.values(shop).join("\n");
+  message.channel.send({
+    "embed": {
+      "url": "https://discordapp.com",
+      "color": 7764732,
+      "timestamp": "2019-01-30T01:24:35.144Z",
+      "footer": {
+        "text": "The Curly Bracket",
+        "url": "https://discord.gg/5kW9EAN",
+        "icon_url": "https://avatars3.githubusercontent.com/u/46971424?s=200&v=4"
+      },
+      "author": {
+        "name": `${message.author.username}`,
+        "icon_url": `${message.author.avatarURL}`
+      },
+      "timestamp": `${new Date()}`,
+      "fields": [{
+          "name": "Product",
+          "value": `${products}`,
+          "inline": true
+        },
+        {
+          "name": "Price",
+          "value": `${prices}`,
+          "inline": true
+        }
+      ]
+    }
+  });
+  return 0; // nonzero value is returned in reply
+}
+
+module.exports.config = { // Command info
+  name: 'list',
+  usage: '$list'
+}
