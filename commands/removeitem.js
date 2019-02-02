94555680d3c1fe36bf3b0a9302d017417e713e79
AlexCheese <alexip013@gmail.com>
2019-02-02 09:54:55 -0600

    remover autocomplete

--- commands/removeitem.js
+++ commands/removeitem.js
@@ -4,18 +4,24 @@ const eliapi = require("eliapi");
 module.exports.run = async (bot, message, args, origin, shop) => { // Runs when command is called
 	if (!message.member.permissions.has(`ADMINISTRATOR`)) return `Missing 'Administrator' permission`;
 	args = [message.content.split(" ").slice(1).join(" ")]
-  if (args.length != 1) return "Invalid command usage";
-  if (!shop[args[0]]) return "Item does not exist";
-  delete shop[args[0]];
-  fs.writeFileSync("shop.json", JSON.stringify(shop, null, 2), "utf8", {
-    if (err) {
-      return eliapi.log(2, err);
-    }
-  });
-  message.channel.send(`Item "${args[0]}" removed from shop`);
-  return 0; // nonzero value is returned in reply
+	if (args[0].length < 2) return "Please provide a valid item name";
+  // if (args.length != 1) return "Invalid command usage";
+  // if (!shop[args[0]]) return "Item does not exist";
+	let shoplist = Object.keys(shop);
+	for (let i in shoplist) {
+		if (shoplist[i].toLowerCase().startsWith(args[0])) {
+			delete shop[shoplist[i]];
+			fs.writeFileSync("shop.json", JSON.stringify(shop, null, 2), "utf8", {
+				if (err) {
+					return eliapi.log(2, err);
+				}
+			});
+			message.channel.send(`Item "${shoplist[i]}" removed from shop`);
+			return 0;
+		}
+	}
+	return 'Item not found';
 }
-
 module.exports.config = { // Command info
   name: 'removeitem',
   usage: '$removeitem [itemname]'
